f5c96af36aab50d746c0c251d1618edd1175da45
Hamza <dev.hamza03@gmail.com>
2026-03-06 22:19:57 +0530

    Reverse a String

--- script.js
+++ script.js
@@ -112,5 +112,12 @@ userObj2.age = 30;
 console.log(userObj2);
 
 
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello")); // "olleh"
+
+
 
 
