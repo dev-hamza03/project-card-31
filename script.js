2b87132626c0d18dfa7dd8e8c763179bac927fd4
Hamza <dev.hamza03@gmail.com>
2026-03-07 22:28:01 +0530

    Find Even Numbers in an Array

--- script.js
+++ script.js
@@ -161,3 +161,10 @@ function removeDuplicates(arr) {
 
 console.log(removeDuplicates([1,2,2,3,4,4,5])); 
 // [1,2,3,4,5]
+
+function getEvenNumbers(arr) {
+  return arr.filter(num => num % 2 === 0);
+}
+
+console.log(getEvenNumbers([1,2,3,4,5,6]));
+// [2,4,6]
